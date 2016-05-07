7ef13a5accca7ce29898be05b40151dbd456f7c5
Ilona Demidenko <demiluri@gmail.com>
2016-05-08 00:57:14 +0200

    refactor: Clean up test suite's common variables.
    Signed-off-by: Oleksii Fedorov <waterlink000@gmail.com>

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -1,29 +1,24 @@
 describe("Tic Tac Toe Game", function() {
-  it("makes sure that player One starts", function() {
-    var game = new Game()
-    var playerOne = "X"
-    var playerTwo = "O"
+  var playerOne = "X"
+  var playerTwo = "O"
 
+  var game
+
+  beforeEach(function() {
+    game = new Game()
+  })
+
+  it("makes sure that player One starts", function() {
     var validTurn = game.put(playerOne)
     expect(validTurn).toEqual(true)
-
   })
 
   it("makes sure that player Two does not start", function() {
-    var game = new Game()
-    var playerOne = "X"
-    var playerTwo = "O"
-
     var validTurn = game.put(playerTwo)
     expect(validTurn).toEqual(false)
-
   })
 
   it("makes sure that player Two makes second turn", function() {
-    var game = new Game()
-    var playerOne = "X"
-    var playerTwo = "O"
-
     game.put(playerOne)
 
     validTurn = game.put(playerTwo)
@@ -31,25 +26,14 @@ describe("Tic Tac Toe Game", function() {
   })
 
   it("is possible to put a mark only in empty cell", function() {
-    var game = new Game()
-    var playerOne = "X"
-    var playerTwo = "O"
-    var cell = [3, 3]
-
-    var validTurn = game.put(playerOne, cell)
+    var validTurn = game.put(playerOne, [3, 3])
     expect(validTurn).toEqual(true)
-
   })
 
   it("is not possible to put mark in occupied cell", function() {
-    var game = new Game()
-    var playerOne = "X"
-    var playerTwo = "O"
-    var cell = [3, 3]
+    game.put(playerOne, [3, 3])
 
-    game.put(playerOne, cell)
-
-    validTurn = game.put(playerTwo, cell)
+    validTurn = game.put(playerTwo, [3, 3])
     expect(validTurn).toEqual(false)
   })
 })
